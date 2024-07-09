504dfb20dc969075053dbbf5b8b1c6f975c822ef
Dave Iberson-Hurst <daveih@dih-m1-mini.home>
2024-07-09 14:45:22 +0200

    Add version table

--- model/models.py
+++ model/models.py
@@ -5,13 +5,13 @@ from .database import Base
 
 class User(Base):
 
-  __tablename__ = "users"
+  __tablename__ = "user"
 
   id = Column(Integer, primary_key=True)
   email = Column(String, unique=True, index=True)
   is_active = Column(Boolean, default=True)
 
-  studies = relationship("Study", back_populates="owner")
+  studies = relationship("Study", back_populates="study_owner")
 
 class Study(Base):
     
@@ -19,7 +19,17 @@ class Study(Base):
 
   id = Column(Integer, primary_key=True)
   name = Column(String, index=True)
-  owner_id = Column(Integer, ForeignKey("users.id"))
+  owner_id = Column(Integer, ForeignKey("user.id"))
 
-  owner = relationship("User", back_populates="studies")
+  versions = relationship("Version", back_populates="version_owner")
+  study_owner = relationship("User", back_populates="studies")
 
+class Version(Base):
+    
+  __tablename__ = "version"
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String, index=True)
+  owner_id = Column(Integer, ForeignKey("study.id"))
+
+  version_owner = relationship("Study", back_populates="versions")
