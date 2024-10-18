36fed9faa90cf7c715246be271dbc87c4fceaa3c
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-10-18 07:06:27 +0200

    Add temporary utility to strip DOCX content to USDM Excel

---
+++ utility_usdm_content.py
@@ -0,0 +1,35 @@
+import argparse
+
+import pandas as pd
+
+from app.model.raw_docx.raw_docx import RawDocx
+
+def save_html(contents, full_path):
+  with open(full_path, 'w', encoding='utf-8') as f:
+    f.write(contents)
+
+def save_excel(doc, full_path):
+  df_content = pd.DataFrame(columns=['name','text'])
+  df_template = pd.DataFrame(columns=['name', 'sectionNumber', 'displaySectionNumber', 'sectionTitle', 'displaySectionTitle', 'content'])
+  for index, section in enumerate(doc.sections):
+    row = {'name': f"NCI_{index+1}", 'text': section.to_html()}
+    df_content.loc[len(df_content)]=row
+  for index, section in enumerate(doc.sections):
+    row = {'name': f"NC_{index+1}", 'sectionNumber': section.number, 'displaySectionNumber': True, 'sectionTitle': section.title, 'displaySectionTitle': True, 'content': f"NCI_{index+1}"}
+    df_template.loc[len(df_template)]=row
+  with pd.ExcelWriter(full_path) as writer:
+    df_content.to_excel(writer, sheet_name="documentContent", header=True, index=False)
+    df_template.to_excel(writer, sheet_name="sponsorTemplate", header=True, index=False)
+    
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(
+    prog="USDM Content From '.docx'",
+    description=''
+  )
+  parser.add_argument('input')
+  parser.add_argument('output')
+  args = parser.parse_args()
+
+  raw_docx = RawDocx(args.input)
+  doc = raw_docx.target_document
+  save_excel(doc, args.output)
