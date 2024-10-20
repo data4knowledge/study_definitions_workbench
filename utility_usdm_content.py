637f5acdcd93545177bac27d5205a477c63d06f3
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-10-20 07:41:46 -0700

    Add section alignment in doc utility

--- utility_usdm_content.py
+++ utility_usdm_content.py
@@ -1,12 +1,15 @@
+import re
 import argparse
-
 import pandas as pd
-
 from app.model.raw_docx.raw_docx import RawDocx
 
 def save_html(contents, full_path):
   with open(full_path, 'w', encoding='utf-8') as f:
     f.write(contents)
+
+def section_number(text):
+  match = re.findall(r'^\d+(?:\.\d+){0,5}', text) if text else None
+  return match[0] if match else None
 
 def save_excel(doc, full_path):
   df_content = pd.DataFrame(columns=['name','text'])
@@ -15,7 +18,13 @@ def save_excel(doc, full_path):
     row = {'name': f"NCI_{index+1}", 'text': section.to_html()}
     df_content.loc[len(df_content)]=row
   for index, section in enumerate(doc.sections):
-    row = {'name': f"NC_{index+1}", 'sectionNumber': section.number, 'displaySectionNumber': True, 'sectionTitle': section.title, 'displaySectionTitle': True, 'content': f"NCI_{index+1}"}
+    sn = section_number(section.title)
+    if sn:
+      st = section.title.replace(sn, '').strip()
+    else:
+      st = section.title
+      sn = ''
+    row = {'name': f"NC_{index+1}", 'sectionNumber': sn, 'displaySectionNumber': True, 'sectionTitle': st, 'displaySectionTitle': True, 'content': f"NCI_{index+1}"}
     df_template.loc[len(df_template)]=row
   with pd.ExcelWriter(full_path) as writer:
     df_content.to_excel(writer, sheet_name="documentContent", header=True, index=False)
