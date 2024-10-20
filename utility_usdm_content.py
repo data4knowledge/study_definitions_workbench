e5cb554e6c5b3773e8527467cf4a661bc387c1ac
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-10-20 16:54:28 -0700

    Improve stripping utility

--- utility_usdm_content.py
+++ utility_usdm_content.py
@@ -12,24 +12,37 @@ def section_number(text):
   return match[0] if match else None
 
 def save_excel(doc, full_path):
+  content = []
+  template = []
   df_content = pd.DataFrame(columns=['name','text'])
   df_template = pd.DataFrame(columns=['name', 'sectionNumber', 'displaySectionNumber', 'sectionTitle', 'displaySectionTitle', 'content'])
-  for index, section in enumerate(doc.sections):
+  merged = merge_sections(doc)
+  for index, section in enumerate(merged):
     row = {'name': f"NCI_{index+1}", 'text': section.to_html()}
-    df_content.loc[len(df_content)]=row
-  for index, section in enumerate(doc.sections):
-    sn = section_number(section.title)
-    if sn:
-      st = section.title.replace(sn, '').strip()
-    else:
-      st = section.title
-      sn = ''
-    row = {'name': f"NC_{index+1}", 'sectionNumber': sn, 'displaySectionNumber': True, 'sectionTitle': st, 'displaySectionTitle': True, 'content': f"NCI_{index+1}"}
-    df_template.loc[len(df_template)]=row
+    df_content.loc[len(df_content)] = row
+  for index, section in enumerate(merged):
+    row = {'name': f"NC_{index+1}", 'sectionNumber': section.number, 'displaySectionNumber': True, 'sectionTitle': section.title, 'displaySectionTitle': True, 'content': f"NCI_{index+1}"}
+    df_template.loc[len(df_template)] = row
   with pd.ExcelWriter(full_path) as writer:
     df_content.to_excel(writer, sheet_name="documentContent", header=True, index=False)
     df_template.to_excel(writer, sheet_name="sponsorTemplate", header=True, index=False)
-    
+
+def merge_sections(doc):
+  sections = []
+  for index, section in enumerate(doc.sections):
+    sn = section_number(section.title)
+    if sn:
+      section.title = section.title.replace(sn, '').strip()
+      section.number = sn
+      sections.append(section)
+    else:
+      if len(sections) > 0:
+        prev_section = sections[-1]
+        prev_section.items = prev_section.items + section.items
+      else:
+        sections.append(section)
+  return sections
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(
     prog="USDM Content From '.docx'",
