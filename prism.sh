fdef115cdf69ac47501aceadfeebd65f424db244
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-10-24 07:27:12 -0700

    Tweak to prism script

--- prism.sh
+++ prism.sh
@@ -1,7 +1,7 @@
 rm .env
 echo ROOT_URL="http://0.0.0.0:8000" >> .env
 echo SINGLE_USER=True >> .env
-echo FILE_PICKER="pfda" >> .env
+echo FILE_PICKER="os" >> .env
 docker image pull data4knowledge/sdw:v0.18.1
 docker volume create sdw_data
 docker run -d --env-file .env --mount source=sdw_data,target=/mount -p 8080:8000 data4knowledge/sdw:v0.18.1 
