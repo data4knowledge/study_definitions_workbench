434561a49b3d3138bea64a549edc3a04fa679ae2
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-10-28 17:35:51 +0100

    Update PRISM script

--- prism.sh
+++ prism.sh
@@ -1,6 +1,9 @@
+pfda download -folder-id 8535631
 rm .env
 echo SINGLE_USER=True >> .env
 echo FILE_PICKER="os" >> .env
-docker image pull data4knowledge/sdw:v0.21.0
+docker image pull data4knowledge/sdw:v0.22.0
 docker volume create sdw_data
-docker run -d --env-file .env --mount source=sdw_data,target=/mount -p 8080:8000 data4knowledge/sdw:v0.21.0
+docker run -d --env-file .env --mount source=sdw_data,target=/mount -p 8080:8000 data4knowledge/sdw:v0.22.0
+mkdir /var/lib/docker/volumes/sdw_data/_data/localfiles
+mv *docx /var/lib/docker/volumes/sdw_data/_data/localfiles
