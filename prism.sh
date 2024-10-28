554b043b80daf1d3d33cec670ce5e1f25c075869
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-10-28 20:15:06 +0100

    Update launch script

--- prism.sh
+++ prism.sh
@@ -1,4 +1,13 @@
-pfda download -folder-id 8535631
+mkdir Sanofi		
+mkdir Lilly Diabetes	
+mkdir Lilly LZZT	
+mkdir Alexion		
+mkdir M11
+pfda download -folder-id 8539458 -output Sanofi		
+pfda download -folder-id 8539459 -output Lilly Diabetes	
+pfda download -folder-id 8539460 -output Lilly LZZT	
+pfda download -folder-id 8539461 -output Alexion		
+pfda download -folder-id 8539507 -output M11
 rm .env
 echo SINGLE_USER=True >> .env
 echo FILE_PICKER="os" >> .env
@@ -6,4 +15,8 @@ docker image pull data4knowledge/sdw:v0.22.0
 docker volume create sdw_data
 docker run -d --env-file .env --mount source=sdw_data,target=/mount -p 8080:8000 data4knowledge/sdw:v0.22.0
 mkdir /var/lib/docker/volumes/sdw_data/_data/localfiles
-mv *docx /var/lib/docker/volumes/sdw_data/_data/localfiles
+mv Sanofi /var/lib/docker/volumes/sdw_data/_data/localfiles
+mv 'Lilly Diabetes' /var/lib/docker/volumes/sdw_data/_data/localfiles
+mv 'Lilly LZZT' /var/lib/docker/volumes/sdw_data/_data/localfiles
+mv Alexion /var/lib/docker/volumes/sdw_data/_data/localfiles		
+mv M11 /var/lib/docker/volumes/sdw_data/_data/localfiles
