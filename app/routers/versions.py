16f9b719cafaa2a1ebfd3dd37335048d84333cdd
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-11-20 12:31:38 +0100

    Add filtering of export menu

--- app/routers/versions.py
+++ app/routers/versions.py
@@ -6,6 +6,7 @@ from app.dependencies.dependency import protect_endpoint
 from app.dependencies.utility import is_fhir_tx, user_details
 from app.dependencies.templates import templates
 from app.model.usdm_json import USDMJson
+from app.dependencies.fhir_version import fhir_versions
 
 router = APIRouter(
   prefix="/versions",
@@ -16,7 +17,6 @@ router = APIRouter(
 @router.get('/{id}/summary')
 async def get_version_summary(request: Request, id: int, session: Session = Depends(get_db)):
   user, present_in_db = user_details(request, session)
-  fhir = is_fhir_tx(request)
   usdm = USDMJson(id, session)
-  data = {'version': usdm.study_version(), 'endpoints': User.endpoints_page(1, 100, user.id, session), 'fhir': fhir}
+  data = {'version': usdm.study_version(), 'endpoints': User.endpoints_page(1, 100, user.id, session), 'fhir': {'enabled': is_fhir_tx(request), 'versions': fhir_versions()}}
   return templates.TemplateResponse(request, "study_versions/summary.html", {'user': user, 'data': data})
