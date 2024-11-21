673684aecf3d04334a8fe7ccc43e3b5901f7ac02
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-11-21 07:59:35 +0100

    First cut FHIR v1 import to upgrade USDM version

--- app/model/fhir/from_fhir_v1.py
+++ app/model/fhir/from_fhir_v1.py
@@ -3,14 +3,16 @@ from usdm_model.study import Study
 from usdm_model.study_design import StudyDesign
 from usdm_model.study_version import StudyVersion
 from usdm_model.study_title import StudyTitle
-from usdm_model.study_protocol_document import StudyProtocolDocument
-from usdm_model.study_protocol_document_version import StudyProtocolDocumentVersion
+# from usdm_model.study_protocol_document import StudyDefinitionDocument
+# from usdm_model.study_protocol_document_version import StudyDefinitionDocumentVersion
+from usdm_model.study_definition_document import StudyDefinitionDocument
+from usdm_model.study_definition_document_version import StudyDefinitionDocumentVersion
 from usdm_model.code import Code
 from usdm_model.alias_code import AliasCode
-from usdm_model.study_identifier import StudyIdentifier
+from usdm_model.identifier import StudyIdentifier
 from usdm_model.organization import Organization
 from usdm_model.address import Address
-from usdm_model.narrative_content import NarrativeContent
+from usdm_model.narrative_content import NarrativeContent, NarrativeContentItem
 from usdm_db.errors_and_logging.errors_and_logging import ErrorsAndLogging
 from usdm_excel.id_manager import IdManager
 from usdm_excel.cdisc_ct_library import CDISCCTLibrary
@@ -21,6 +23,7 @@ from app import SYSTEM_NAME, VERSION
 from app.model.file_handling.data_files import DataFiles
 from app.model.fhir.fhir_title_page import FHIRTitlePage
 from d4kms_generic.logger import application_logger
+from app.model.m11_protocol.m11_utility import language_code
 
 class FromFHIRV1():
 
@@ -46,33 +49,44 @@ class FromFHIRV1():
   
   def _from_fhir(self, uuid: str, data: str) -> Wrapper:
     bundle = Bundle.parse_raw(data)
-    protocol_document = self._document(bundle)
-    study = self._study(protocol_document)
+    protocol_document, ncis = self._document(bundle)
+    study = self._study(protocol_document, ncis)
     return study
 
   def _document(self, bundle):
     self._ncs = []
     protocl_status_code = self._cdisc_ct_code('C85255', 'Draft')
-    protocl_document_version = self._model_instance(StudyProtocolDocumentVersion, {'protocolVersion': '1', 'protocolStatus': protocl_status_code})
-    protocl_document = self._model_instance(StudyProtocolDocument, {'name': 'PROTOCOL V1', 'label': '', 'description': '', 'versions': [protocl_document_version]})
-    root = self._model_instance(NarrativeContent, {'name': 'ROOT', 'sectionNumber': '0', 'sectionTitle': 'Root', 'text': '', 'childIds': [], 'previousId': None, 'nextId': None})
-    protocl_document_version.contents.append(root)
+    protocl_document_version = self._model_instance(StudyDefinitionDocumentVersion, {'version': '1', 'status': protocl_status_code})
+    language = language_code('en', 'English', self._id_manager)
+    doc_type = self._cdisc_ct_code('C70817', 'Protocol')
+    protocl_document = self._model_instance(StudyDefinitionDocument, {'name': 'PROTOCOL V1', 'label': 'M11 Protocol', 'description': 'M11 Protocol Document', 'language': language, 'type': doc_type, 'templateName': 'M11', 'versions': [protocl_document_version]})
+    #root = self._model_instance(NarrativeContent, {'name': 'ROOT', 'sectionNumber': '0', 'sectionTitle': 'Root', 'text': '', 'childIds': [], 'previousId': None, 'nextId': None})
+    #protocl_document_version.contents.append(root)
+    ncis = []
     for item in bundle.entry[0].resource.section:
-      nc = self._section(item, protocl_document_version)
-      root.childIds.append(nc.id)
-    self._double_link(protocl_document_version.contents, 'previousId', 'nextId')
+      nc = self._section(item, protocl_document_version, ncis, 0)
+    #self._double_link(protocl_document_version.contents, 'previousId', 'nextId')
     #print(f"DOC: {protocl_document}")
-    return protocl_document
+    return protocl_document, ncis
 
-  def _section(self, section: CompositionSection, protocol_document_version: StudyProtocolDocumentVersion):
-    #print(f"SECTION: {section.title}, {section.code.text}")
+  def _section(self, section: CompositionSection, protocol_document_version: StudyDefinitionDocumentVersion, ncis: list, index: int):
+    index = index + 1
+    print(f"SECTION: {section.title}, {section.code.text}")
     section_number = self._get_section_number(section.code.text)
-    #print(f"SECTION NUMBER: {section.code.text} -> {section_number}")
-    nc = self._model_instance(NarrativeContent, {'name': f"{section.code.text}", 'sectionNumber': section_number, 'sectionTitle': section.title, 'text': section.text.div, 'childIds': [], 'previousId': None, 'nextId': None})
+    section_title = section.title
+    sn = section_number if section_number else ''
+    dsn = True if sn else False
+    st = section_title if section_title else ''
+    dst = True if st else False
+    print(f"SECTION: sn='{sn}', dsn='{dsn}', st='{st}', dst='{dst}'")
+    text = section.text.div if section.text else '&nbsp'
+    nci = self._model_instance(NarrativeContentItem, {'name': f"NCI-{index}", 'text': text})
+    nc = self._model_instance(NarrativeContent, {'name': f"NC-{index}", 'sectionNumber': sn, 'displaySectionNumber': dsn, 'sectionTitle': st, 'displaySectionTitle': dst, 'contentItemId': nci.id, 'childIds': [], 'previousId': None, 'nextId': None})
     protocol_document_version.contents.append(nc)
+    ncis.append(nci)
     if section.section:
       for item in section.section:
-        child_nc = self._section(item, protocol_document_version)
+        child_nc = self._section(item, protocol_document_version, ncis, index)
         nc.childIds.append(child_nc.id)
     return nc
 
@@ -80,29 +94,39 @@ class FromFHIRV1():
     parts = text.split('-')
     return parts[0].replace('section', '') if len(parts) >= 2 else ''
       
-  def _study(self, protocol_document: StudyProtocolDocument):
+  def _study(self, protocol_document: StudyDefinitionDocument, ncis: list):
     protocol_document_version = protocol_document.versions[0]
     #print(f"PDV: {protocol_document_version}")
     sections = protocol_document_version.contents
     #print(f"SECTION: {protocol_document_version}")
-    title_page = FHIRTitlePage(sections[1].text)
+    nci = next((x for x in ncis if x.id == sections[0].contentItemId), None)
+    print(f"TITLE: {nci}")
+    title_page = FHIRTitlePage(nci.text)
     sponsor_title_code = self._cdisc_ct_code('C99905x2', 'Official Study Title')
     study_title = self._model_instance(StudyTitle, {'text': title_page.full_title, 'type': sponsor_title_code})
     protocl_status_code = self._cdisc_ct_code('C85255', 'Draft')
     intervention_model_code = self._cdisc_ct_code('C82639', 'Parallel Study')
     country_code = self._iso_country_code('DNK', 'Denmark')
     sponsor_code = self._cdisc_ct_code("C70793", 'Clinical Study Sponsor')
-    #protocol_document_version = self._model_instance(StudyProtocolDocumentVersion, {'protocolVersion': title_page.version_number, 'protocolStatus': protocl_status_code})
-    #protocol_document = self._model_instance(StudyProtocolDocument, {'name': f'PROTOCOL_DOCUMENT', 'label': f'Protocol Document', 'description': 'The protocol document for the study', 'versions': [protocol_document_version]})
     study_design = self._model_instance(StudyDesign, {'name': 'Study Design', 'label': '', 'description': '', 
       'rationale': '[Not Found]', 'interventionModel': intervention_model_code, 'arms': [], 'studyCells': [], 
       'epochs': [], 'population': None})
     address = self._model_instance(Address, {'line': 'Den Lille Havfrue', 'city': 'Copenhagen', 'district': '', 'state': '', 'postalCode': '12345', 'country': country_code})
-    organization = self._model_instance(Organization, {'name': title_page.sponsor_name, 'organizationType': sponsor_code, 'identifier': "123456789", 'identifierScheme': "DUNS", 'legalAddress': address}) 
-    identifier = self._model_instance(StudyIdentifier, {'studyIdentifier': title_page.sponsor_protocol_identifier, 'studyIdentifierScope': organization})
-    study_version = self._model_instance(StudyVersion, {'versionIdentifier': title_page.version_number, 'rationale': 'XXX', 'titles': [study_title], 'studyDesigns': [study_design], 
-                                                     'documentVersionId': protocol_document_version.id, 'studyIdentifiers': [identifier], 'studyPhase': self._phase(title_page.trial_phase)}) 
-    study = self._model_instance(Study, {'id': self._uuid, 'name': 'Study', 'label': '', 'description': '', 'versions': [study_version], 'documentedBy': protocol_document}) 
+    organization = self._model_instance(Organization, {'name': title_page.sponsor_name, 'type': sponsor_code, 'identifier': "123456789", 'identifierScheme': "DUNS", 'legalAddress': address}) 
+    identifier = self._model_instance(StudyIdentifier, {'text': title_page.sponsor_protocol_identifier, 'scopeId': organization.id})
+    print(f"IDENTIFIER: {identifier}")
+    params = {
+      'versionIdentifier': title_page.version_number, 
+      'rationale': 'XXX', 
+      'titles': [study_title], 
+      'studyDesigns': [study_design], 
+      'documentVersionId': protocol_document_version.id, 
+      'studyIdentifiers': [identifier], 
+      'studyPhase': self._phase(title_page.trial_phase), 
+      'organizations': [organization]
+    }
+    study_version = self._model_instance(StudyVersion, params) 
+    study = self._model_instance(Study, {'id': self._uuid, 'name': 'Study', 'label': '', 'description': '', 'versions': [study_version], 'documentedBy': [protocol_document]}) 
     return study
 
   def _cdisc_ct_code(self, code, decode):
