449393eab9f31eadf1f4f4c5112c54bc4feb8827
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-11-21 14:56:57 +0100

    New test

--- tests/fhir/test_integration.py
+++ tests/fhir/test_integration.py
@@ -27,5 +27,8 @@ def _full_path(filename):
   return f"tests/test_files/FHIR-v1/{filename}"
 
 
-def test_fhir_v1_1(mocker):
+def test_fhir_v1_ASP8062(mocker):
   _run_test('ASP8062', True)
+
+def test_fhir_v1_DEUCRALIP(mocker):
+  _run_test('DEUCRALIP', True)
