e800a3ebb23838b8f5ce4025c2b376f4fe4f59ff
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-12-13 14:32:29 +0100

    Split off studies into a router

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 
-from typing import Annotated
 from fastapi import Form, Depends, FastAPI, Request, WebSocket, WebSocketDisconnect, status
 from fastapi.responses import RedirectResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
@@ -24,12 +23,11 @@ from app.dependencies.fhir_version import check_fhir_version, fhir_versions
 from app.utility.fhir_transmit import run_fhir_transmit
 from app.model.database_manager import DatabaseManager as DBM
 from app.model.exceptions import FindException
-from app.model.usdm.m11.title_page import USDMM11TitlePage
 from app.model.file_handling.pfda_files import PFDAFiles
 from app.model.file_handling.local_files import LocalFiles
 from app.model.unified_diff.unified_diff import UnifiedDiff
 
-from app.routers import transmissions, users, versions, help
+from app.routers import transmissions, users, versions, help, studies
 from app.dependencies.dependency import set_middleware_secret, protect_endpoint, authorisation
 from app.dependencies.utility import user_details, single_user, is_admin, is_fhir_tx
 from app.dependencies.templates import templates, templates_path
@@ -52,6 +50,7 @@ set_middleware_secret(app)
 app.include_router(users.router)
 app.include_router(transmissions.router)
 app.include_router(versions.router)
+app.include_router(studies.router)
 app.include_router(help.router)
 
 @app.exception_handler(Exception)
@@ -106,47 +105,47 @@ def index(request: Request, session: Session = Depends(get_db)):
   else:
     return templates.TemplateResponse(request, 'errors/error.html', {'user': None, 'data': {'error': "Unable to determine user."}})
 
-@app.patch("/studies/{id}/select", dependencies=[Depends(protect_endpoint)])
-def study_select(request: Request, id: int, action: str, list_studies: Annotated[str, Form()]=None, session: Session = Depends(get_db)):
-  # data = {}
-  user, present_in_db = user_details(request, session)
-  selected = True if action.upper() == 'SELECT' else False
-  parts = list_studies.split(',') if list_studies else []
-  parts = [x.strip() for x in parts]
-  parts.append(str(id)) if selected else parts.remove(str(id))
-  data = {'study': Study.summary(id, session), 'selected': selected, 'selected_list': (',').join(parts)}
-  return templates.TemplateResponse(request, "studies/partials/select.html", {'user': user, 'data': data})
+# @app.patch("/studies/{id}/select", dependencies=[Depends(protect_endpoint)])
+# def study_select(request: Request, id: int, action: str, list_studies: Annotated[str, Form()]=None, session: Session = Depends(get_db)):
+#   # data = {}
+#   user, present_in_db = user_details(request, session)
+#   selected = True if action.upper() == 'SELECT' else False
+#   parts = list_studies.split(',') if list_studies else []
+#   parts = [x.strip() for x in parts]
+#   parts.append(str(id)) if selected else parts.remove(str(id))
+#   data = {'study': Study.summary(id, session), 'selected': selected, 'selected_list': (',').join(parts)}
+#   return templates.TemplateResponse(request, "studies/partials/select.html", {'user': user, 'data': data})
 
-@app.post("/studies/delete", dependencies=[Depends(protect_endpoint)])
-def study_delete(request: Request, delete_studies: Annotated[str, Form()]=None, session: Session = Depends(get_db)):
-  #user, present_in_db = user_details(request, session)
-  parts = delete_studies.split(',') if delete_studies else []
-  for id in parts:
-    study = Study.find(id, session)
-    imports = study.file_imports(session)
-    for im in imports:
-      files = DataFiles(im[1])
-      files.delete()
-      x = FileImport.find(im[0], session)
-      x.delete(session)
-    study.delete(session)
-  return RedirectResponse("/index", status_code=status.HTTP_303_SEE_OTHER)
+# @app.post("/studies/delete", dependencies=[Depends(protect_endpoint)])
+# def study_delete(request: Request, delete_studies: Annotated[str, Form()]=None, session: Session = Depends(get_db)):
+#   #user, present_in_db = user_details(request, session)
+#   parts = delete_studies.split(',') if delete_studies else []
+#   for id in parts:
+#     study = Study.find(id, session)
+#     imports = study.file_imports(session)
+#     for im in imports:
+#       files = DataFiles(im[1])
+#       files.delete()
+#       x = FileImport.find(im[0], session)
+#       x.delete(session)
+#     study.delete(session)
+#   return RedirectResponse("/index", status_code=status.HTTP_303_SEE_OTHER)
 
-@app.get("/studies/list", dependencies=[Depends(protect_endpoint)])
-def study_list(request: Request, list_studies: str=None, session: Session = Depends(get_db)):
-  user, present_in_db = user_details(request, session)
-  #print(f"STUDIES: {list_studies}")
-  parts = list_studies.split(',') if list_studies else []
-  data = []
-  for id in parts:
-    version = Version.find_latest_version(id, session)
-    usdm = USDMJson(version.id, session)
-    m11 = USDMM11TitlePage(usdm.wrapper(), usdm.extra())
-    data.append(m11.__dict__)
-  data = restructure_study_list(data)
-  #print(f"STUDY LIST: {data}")
-  data['fhir'] = {'enabled': is_fhir_tx(request), 'versions': fhir_versions()}
-  return templates.TemplateResponse(request, "studies/list.html", {'user': user, 'data': data})
+# @app.get("/studies/list", dependencies=[Depends(protect_endpoint)])
+# def study_list(request: Request, list_studies: str=None, session: Session = Depends(get_db)):
+#   user, present_in_db = user_details(request, session)
+#   #print(f"STUDIES: {list_studies}")
+#   parts = list_studies.split(',') if list_studies else []
+#   data = []
+#   for id in parts:
+#     version = Version.find_latest_version(id, session)
+#     usdm = USDMJson(version.id, session)
+#     m11 = USDMM11TitlePage(usdm.wrapper(), usdm.extra())
+#     data.append(m11.__dict__)
+#   data = restructure_study_list(data)
+#   #print(f"STUDY LIST: {data}")
+#   data['fhir'] = {'enabled': is_fhir_tx(request), 'versions': fhir_versions()}
+#   return templates.TemplateResponse(request, "studies/list.html", {'user': user, 'data': data})
 
 @app.get("/fileList", dependencies=[Depends(protect_endpoint)])
 def file_list(request: Request, dir: str, url: str, session: Session = Depends(get_db)):
---
+++ app/routers/studies.py
@@ -0,0 +1,65 @@
+from typing import Annotated
+from fastapi import APIRouter, Form, Depends, Request, status
+from fastapi.responses import RedirectResponse
+from sqlalchemy.orm import Session
+from app.model.study import Study
+from app.model.version import Version
+from app.model.file_import import FileImport
+from app.model.database import get_db
+from app.model.usdm.m11.title_page import USDMM11TitlePage
+from app.model.usdm_json import USDMJson
+from app.model.file_handling.data_files import DataFiles
+from app.dependencies.dependency import protect_endpoint
+from app.dependencies.utility import is_fhir_tx, user_details
+from app.dependencies.templates import templates
+from app.utility.template_methods import restructure_study_list
+from app.dependencies.fhir_version import fhir_versions
+from d4kms_generic.logger import application_logger
+
+router = APIRouter(
+  prefix="/studies",
+  tags=["studies"],
+  dependencies=[Depends(protect_endpoint)]
+)
+
+@router.patch("/{id}/select", dependencies=[Depends(protect_endpoint)])
+def study_select(request: Request, id: int, action: str, list_studies: Annotated[str, Form()]=None, session: Session = Depends(get_db)):
+  # data = {}
+  user, present_in_db = user_details(request, session)
+  selected = True if action.upper() == 'SELECT' else False
+  parts = list_studies.split(',') if list_studies else []
+  parts = [x.strip() for x in parts]
+  parts.append(str(id)) if selected else parts.remove(str(id))
+  data = {'study': Study.summary(id, session), 'selected': selected, 'selected_list': (',').join(parts)}
+  return templates.TemplateResponse(request, "studies/partials/select.html", {'user': user, 'data': data})
+
+@router.post("/delete", dependencies=[Depends(protect_endpoint)])
+def study_delete(request: Request, delete_studies: Annotated[str, Form()]=None, session: Session = Depends(get_db)):
+  #user, present_in_db = user_details(request, session)
+  parts = delete_studies.split(',') if delete_studies else []
+  for id in parts:
+    study = Study.find(id, session)
+    imports = study.file_imports(session)
+    for im in imports:
+      files = DataFiles(im[1])
+      files.delete()
+      x = FileImport.find(im[0], session)
+      x.delete(session)
+    study.delete(session)
+  return RedirectResponse("/index", status_code=status.HTTP_303_SEE_OTHER)
+
+@router.get("/list", dependencies=[Depends(protect_endpoint)])
+def study_list(request: Request, list_studies: str=None, session: Session = Depends(get_db)):
+  user, present_in_db = user_details(request, session)
+  #print(f"STUDIES: {list_studies}")
+  parts = list_studies.split(',') if list_studies else []
+  data = []
+  for id in parts:
+    version = Version.find_latest_version(id, session)
+    usdm = USDMJson(version.id, session)
+    m11 = USDMM11TitlePage(usdm.wrapper(), usdm.extra())
+    data.append(m11.__dict__)
+  data = restructure_study_list(data)
+  #print(f"STUDY LIST: {data}")
+  data['fhir'] = {'enabled': is_fhir_tx(request), 'versions': fhir_versions()}
+  return templates.TemplateResponse(request, "studies/list.html", {'user': user, 'data': data})
