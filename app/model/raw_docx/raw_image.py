29b60e47bfd145526607ef20635a704434d598c0
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2024-12-22 09:15:55 +0100

    Error handling for inalid image types, eg '.emf'

--- app/model/raw_docx/raw_image.py
+++ app/model/raw_docx/raw_image.py
@@ -12,12 +12,15 @@ class RawImage():
   def to_html(self):
     try:
       file_root, file_extension = os.path.splitext(self.filepath)
-      with open(self.filepath, "rb") as image_file:
-        data = base64.b64encode(image_file.read())
-      file_type = self.FILE_TYPE_MAP[file_extension]
-      decoded = data.decode("ascii")
-      return f"<img alt=\"alt text\" src=\"data:image/{file_type};base64,{decoded}\"/>"
+      if file_extension in self.FILE_TYPE_MAP:
+        file_type = self.FILE_TYPE_MAP[file_extension]
+        with open(self.filepath, "rb") as image_file:
+          data = base64.b64encode(image_file.read())
+        decoded = data.decode("ascii")
+        return f"<img alt=\"alt text\" src=\"data:image/{file_type};base64,{decoded}\"/>"
+      else:
+        return f"""<p style="color:red">Note: Unable to process embedded image of type '{file_extension}', image ignored.</p>"""
     except Exception as e:
       application_logger.exception("Exception converting image", e)
-      return ""
+      return f"""<p style="color:red">Note: Error encountered processing image.</p>"""
 
