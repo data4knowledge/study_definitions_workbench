54956473f5d3cba43bc65bc63d9b2c54aa8e20e1
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-01-01 11:36:54 +0100

    Add related action

---
+++ app/usdm/fhir/factory/plan_definition_releated_action_factory.py
@@ -0,0 +1,11 @@
+from app.usdm.fhir.factory.base_factory import BaseFactory
+from fhir.resources.plandefinition import PlanDefinitionActionRelatedAction
+
+class PlanDefinitionRelatedActionFactory(BaseFactory):
+  
+  def __init__(self, **kwargs):
+    try: 
+      self.item = PlanDefinitionActionRelatedAction(**kwargs)
+    except Exception as e:
+      self.item = None
+      self.handle_exception(e)
