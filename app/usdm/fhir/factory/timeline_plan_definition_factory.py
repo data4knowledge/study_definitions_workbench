bf4bce39bf667c6cf2fd1dd2be8ddfabba4a238f
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-01-01 14:39:06 +0100

    More details

--- app/usdm/fhir/factory/timeline_plan_definition_factory.py
+++ app/usdm/fhir/factory/timeline_plan_definition_factory.py
@@ -5,6 +5,7 @@ from app.usdm.fhir.factory.coding_factory import CodingFactory
 from app.usdm.fhir.factory.codeable_concept_factory import CodeableConceptFactory
 from app.usdm.fhir.factory.plan_definition_action_factory import PlanDefinitionActionFactory
 from app.usdm.fhir.factory.plan_definition_related_action_factory import PlanDefinitionRelatedActionFactory
+from app.usdm.fhir.factory.extension_factory import ExtensionFactory
 from app.usdm.fhir.factory.iso8601_ucum import ISO8601ToUCUM
 from usdm_model.schedule_timeline import ScheduleTimeline
 from usdm_model.scheduled_instance import ScheduledActivityInstance, ScheduledDecisionInstance
@@ -38,43 +39,8 @@ class TimelinePlanDefinitionFactory(BaseFactory):
   def _related_action(self, timeline: ScheduleTimeline, timepoint: ScheduledDecisionInstance | ScheduledActivityInstance) -> dict:
     timing: Timing = timeline.find_timing_from(timepoint.id)
     offset = ISO8601ToUCUM.convert(timing.value)
-    related = PlanDefinitionRelatedActionFactory(targetId=self.fix_id(timing.id), relationship=timing.type.decode, offsetDuration=offset)
+    related = PlanDefinitionRelatedActionFactory(targetId=self.fix_id(timing.id), relationship=timing.type.decode, offsetDuration=offset, extension=[])
+    if timing.windowLower:
+      window = ExtensionFactory(**{'valueRange': {'low': ISO8601ToUCUM.convert(timing.windowLower), 'high': ISO8601ToUCUM.convert(timing.windowUpper)}})
+      related.item.extension.append(window.item)
     return related.item
-  
-
-
-
-  # class ConditionAssignment(ApiBaseModelWithId):
-  # condition: str
-  # conditionTargetId: str
-  # instanceType: Literal['ConditionAssignment']
-# 
-# class ScheduledInstance(ApiBaseModelWithIdNameLabelAndDesc):
-  # defaultConditionId: Union[str, None] = None
-  # epochId: Union[str, None] = None
-  # instanceType: Literal['ScheduledInstance']
-# 
-# class ScheduledActivityInstance(ScheduledInstance):
-  # timelineId: Union[str, None] = None
-  # timelineExitId: Union[str, None] = None
-  # activityIds: List[str] = []
-  # encounterId: Union[str, None] = None
-  # instanceType: Literal['ScheduledActivityInstance']
-# 
-# class ScheduledDecisionInstance(ScheduledInstance):
-# conditionAssignments: List[ConditionAssignment] = [] # Allow for empty list, not in API
-# instanceType: Literal['ScheduledDecisionInstance']
-# 
-# 
-
-# class Timing(ApiBaseModelWithIdNameLabelAndDesc):
-#   type: Code
-#   value: str
-#   valueLabel: str
-#   relativeToFrom: Code
-#   relativeFromScheduledInstanceId: str
-#   relativeToScheduledInstanceId: Union[str, None] = None
-#   windowLower: Union[str, None] = None
-#   windowUpper: Union[str, None] = None
-#   windowLabel: Union[str, None] = None
-#   instanceType: Literal['Timing']  
