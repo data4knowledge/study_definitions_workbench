3ccbfb3dcf292f9256075e361ad42e2e9ba5ff4e
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-01-06 16:54:07 +0100

    Remove print statements

--- app/routers/version_timelines.py
+++ app/routers/version_timelines.py
@@ -18,7 +18,7 @@ async def get_study_design_timelines(request: Request, version_id: int, study_de
   user, present_in_db = user_details(request, session)
   usdm = USDMJson(version_id, session)
   data = usdm.timelines(study_design_id)
-  print(f"DATA: {data}")
+  #print(f"DATA: {data}")
   return templates.TemplateResponse(request, "study_designs/partials/timelines.html", {'user': user, 'data': data})
 
 @router.get('/{version_id}/studyDesigns/{study_design_id}/timelines/{timeline_id}/soa', dependencies=[Depends(protect_endpoint)])
@@ -26,7 +26,7 @@ async def get_study_design_timeline_soa(request: Request, version_id: int, study
   user, present_in_db = user_details(request, session)
   usdm = USDMJson(version_id, session)
   data = usdm.soa(study_design_id, timeline_id)
-  print(f"DATA: {data}")
+  #print(f"DATA: {data}")
   return templates.TemplateResponse(request, "timelines/soa.html", {'user': user, 'data': data})
 
 @router.get('/{version_id}/studyDesigns/{study_design_id}/timelines/{timeline_id}/export/fhir', dependencies=[Depends(protect_endpoint)])
