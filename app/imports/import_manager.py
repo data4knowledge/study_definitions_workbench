cb581ee553191d21f7355dc25ce7100153f7cb9f
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-02-08 15:30:33 +0100

    Improve the import manager

--- app/imports/import_manager.py
+++ app/imports/import_manager.py
@@ -24,27 +24,43 @@ def execute_import(type: str, uuid: str, user: User) -> None:
 
 
 class ImportManager:
-    PROCESSOR_MAPPING = {
-        "xlsx": {"processor": ImportExcel, "files": "xlsx"},
-        "docx": {"processor": ImportWord, "files": "docx"},
-        "fhir": {"processor": ImportFhirV1, "files": "fhir"},
-        "usdm3": {"processor": ImportUSDM3, "files": "usdm"},
-        "usdm4": {"processor": ImportUSDM4, "files": "usdm"},
-    }
+    USDM_EXCEL = "USDM_EXCEL"
+    M11_DOCX = "M11_DOCX"
+    FHIR_V1_JSON = "FHIR_V1_JSON"
+    #FHIR_V2_JSON = "FHIR_V2_JSON"
+    USDM3_JSON = "USDM3_JSON"
+    USDM4_JSON = "USDM4_JSON"
 
     def __init__(self, uuid: str, user: User, type: str) -> None:
+        self.mapping = {
+            self.USDM_EXCEL: {"processor": ImportExcel, "main_file_type": "xlsx", 'main_file_ext': '.json', "images": True},
+            self.M11_DOCX: {"processor": ImportWord, "main_file_type": "docx", 'main_file_ext': '.docx', "images": False},
+            self.FHIR_V1_JSON: {"processor": ImportFhirV1, "main_file_type": "fhir", 'main_file_ext': '.json', "images": False},
+            self.USDM3_JSON: {"processor": ImportUSDM3, "main_file_type": "usdm", 'main_file_ext': '.json', "images": False},
+            self.USDM4_JSON: {"processor": ImportUSDM4, "main_file_type": "usdm", 'main_file_ext': '.json', "images": False},
+        }
         self.uuid = uuid
         self.user = user
         self.type = type
-        self.processor = self.PROCESSOR_MAPPING[type]["processor"]
-        self.file_type = self.PROCESSOR_MAPPING[type]["files"]
+        self.processor = self.mapping[type]["processor"]
+        self.main_file_type = self.mapping[type]["main_file_type"]
+        self.main_file_ext = self.mapping[type]["main_file_ext"]
+        self.images = self.mapping[type]["images"]
+        self.files = None
+
+    def save_files(self, main_file: dict, image_files: dict) -> str:
+        self.files = DataFiles()
+        uuid = self.files.new()
+        self._save_file(self.files, main_file, self.main_file_type)
+        for image_file in image_files:
+            self._save_file(self.files, image_file, "image")
+        return uuid
 
     async def process(self) -> None:
         try:
             session = SessionLocal()
             file_import = None
-            files = DataFiles(self.uuid)
-            full_path, filename, exists = files.path(self.file_type)
+            full_path, filename, exists = self.files.path(self.type)
             file_import = FileImport.create(
                 full_path,
                 filename,
@@ -59,9 +75,9 @@ class ImportManager:
             
             file_import.update_status("Saving", session)
             if processor.errors:
-                files.save("errors", processor.errors)
-            files.save("usdm", processor.usdm)
-            files.save("extra", processor.extra)
+                self.files.save("errors", processor.errors)
+            self.files.save("usdm", processor.usdm)
+            self.files.save("extra", processor.extra)
 
             file_import.update_status("Create", session)
             Study.study_and_version(parameters, self.user, file_import, session)
@@ -78,3 +94,8 @@ class ImportManager:
             await connection_manager.error(
                 "Error encountered importing", str(self.user.id)
             )
+
+    def _save_file(self, file_details: dict, file_type: str) -> tuple[str, str]:
+        filename = file_details["filename"]
+        contents = file_details["contents"]
+        return self.files.save(file_type, contents, filename)
