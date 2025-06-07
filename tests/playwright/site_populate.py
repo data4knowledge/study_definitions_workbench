05ef20c9dca0654089e75c28da494d8b82ba693e
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-06-07 21:04:31 +0200

    Tweak site population to add page load rates and longer waits

--- tests/playwright/site_populate.py
+++ tests/playwright/site_populate.py
@@ -3,8 +3,8 @@ import pytest
 from d4k_ms_base.service_environment import ServiceEnvironment
 from playwright.sync_api import Playwright, expect
 
-# site_url = f"https://d4k-sdw-staging.fly.dev"
-site_url = f"https://d4k-sdw.fly.dev"
+site_url = f"https://d4k-sdw-staging.fly.dev"
+# site_url = f"https://d4k-sdw.fly.dev"
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -23,7 +23,6 @@ def test_populate(playwright: Playwright) -> None:
     page.goto(site_url)
 
     login(page)
-    # delete_db(page)
 
     load_m11(page, path, "tests/test_files/m11/WA42380/WA42380.docx")
     load_m11(page, path, "tests/test_files/m11/ASP8062/ASP8062.docx")
@@ -73,48 +72,45 @@ def login(page):
 
 
 def logout(page):
-    page.get_by_role("link").first.click()
+    home_page_wait(page)
     page.get_by_role("link", name=" Logout").click()
 
 
 def load_excel(page, root_path, filepath):
-    page.get_by_role("link").first.click()
+    home_page_wait(page)
     page.get_by_role("button", name=" Import").click()
     page.get_by_role("link", name="USDM Excel (.xlsx)").click()
     page.set_input_files("#files", os.path.join(root_path, filepath))
     page.locator("text = Upload File(s)").last.click()
-    expect(page.get_by_text("Success: Import of")).to_be_visible(timeout=30_000)
+    expect(page.get_by_text("Success: Import of")).to_be_visible(timeout=60_000)
 
 
 def load_m11(page, root_path, filepath):
-    page.get_by_role("link").first.click()
+    home_page_wait(page)
     page.get_by_role("button", name=" Import").click()
     page.get_by_role("link", name="M11 Document (.docx)").click()
     page.set_input_files("#files", os.path.join(root_path, filepath))
     page.locator("text = Upload File(s)").last.click()
-    expect(page.get_by_text("Success: Import of")).to_be_visible(timeout=30_000)
+    expect(page.get_by_text("Success: Import of")).to_be_visible(timeout=60_000)
 
 
 def load_fhir(page, root_path, filepath):
-    page.get_by_role("link").first.click()
+    home_page_wait(page)
     page.get_by_role("button", name=" Import").click()
     page.get_by_role("link", name="M11 FHIR v1, Dallas 2024").click()
     page.set_input_files("#files", os.path.join(root_path, filepath))
     page.locator("text = Upload File(s)").last.click()
-    expect(page.get_by_text("Success: Import of")).to_be_visible(timeout=30_000)
+    expect(page.get_by_text("Success: Import of")).to_be_visible(timeout=60_000)
 
 
 def load_usdm(page, root_path, filepath, version, result):
-    page.get_by_role("link").first.click()
+    home_page_wait(page)
     page.get_by_role("button", name=" Import").click()
     page.get_by_role(f"link", name=f"USDM v{version} (.json)").click()
     page.set_input_files("#files", os.path.join(root_path, filepath))
     page.locator("text = Upload File(s)").last.click()
-    expect(page.get_by_text(f"{result}")).to_be_visible(timeout=30_000)
+    expect(page.get_by_text(f"{result}")).to_be_visible(timeout=60_000)
 
-
-def delete_db(page):
-    page.get_by_role("link", name=f" {display_name()}").click()
-    page.once("dialog", lambda dialog: dialog.accept())
-    page.get_by_role("link", name=" Delete Database").click()
-    page.get_by_role("link", name=" Home").click()
+def home_page_wait(page):
+    page.get_by_role("link").first.click()
+    page.wait_for_load_state('domcontentloaded')
