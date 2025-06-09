78329a2afd23c067efaf9166e5b3ebaa3604fc66
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-06-09 14:24:37 +0200

    Update M11 tests

--- tests/m11/test_m11_integration.py
+++ tests/m11/test_m11_integration.py
@@ -2,10 +2,9 @@ import re
 import pytest
 from tests.files.files import *
 from app.model.file_handling.data_files import DataFiles
-from app.model.m11_protocol.m11_protocol import M11Protocol
-from app import VERSION, SYSTEM_NAME
+from usdm4_m11 import USDM4M11
 
-WRITE_FILE = False
+SAVE = False
 
 
 @pytest.fixture
@@ -20,11 +19,9 @@ async def _run_test(dir, name, save=False):
     uuid = files.new()
     files.save("docx", contents, filename)
     filepath, filename, media = files.path("docx")
-    m11 = M11Protocol(filepath, SYSTEM_NAME, VERSION)
-    await m11.process()
-    result = m11.to_usdm()
+    m11 = USDM4M11()
+    result = await m11.from_docx(filepath)
     result = replace_uuid(result)
-    # print(f"MATCH: {result[10:70]}")
     pretty_result = json.dumps(json.loads(result), indent=2)
     result_filename = filename = f"{name}_usdm.json"
     if save:
@@ -39,22 +36,22 @@ def _full_path(dir, filename):
 
 @pytest.mark.anyio
 async def test_m11_radvax():
-    await _run_test("RadVax", "RadVax", WRITE_FILE)
+    await _run_test("RadVax", "RadVax", SAVE)
 
 
 @pytest.mark.anyio
 async def test_m11_wa42380():
-    await _run_test("WA42380", "WA42380", WRITE_FILE)
+    await _run_test("WA42380", "WA42380", SAVE)
 
 
 @pytest.mark.anyio
 async def test_m11_lzzt():
-    await _run_test("LZZT", "LZZT", WRITE_FILE)
+    await _run_test("LZZT", "LZZT", SAVE)
 
 
 @pytest.mark.anyio
 async def test_m11_asp8062():
-    await _run_test("ASP8062", "ASP8062", WRITE_FILE)
+    await _run_test("ASP8062", "ASP8062", SAVE)
 
 
 def replace_uuid(result):
