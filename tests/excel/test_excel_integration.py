856397a92d3965342c21541482a58cb9a2bfdc9d
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-07-20 17:44:40 +0200

    Fix test save

--- tests/excel/test_excel_integration.py
+++ tests/excel/test_excel_integration.py
@@ -2,7 +2,7 @@ from tests.files.files import *
 from usdm_db import USDMDb
 from app.model.file_handling.data_files import DataFiles
 
-SAVE = True
+SAVE = False
 
 
 def _run_test(name, save=False):
