fc924c1337dad36a75e05fa59c06e7d66132fd31
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-09-21 17:24:35 +0200

    Tidy up code

--- app/usdm_database/usdm_database.py
+++ app/usdm_database/usdm_database.py
@@ -18,13 +18,6 @@ class USDMDatabase:
         self.uuid = file_import.uuid
         self.type = file_import.type
         self._files = DataFiles(file_import.uuid)
-        # self.m11 = (
-        #     True
-        #     if self.type in [ImportManager.M11_DOCX, ImportManager.FHIR_V1_JSON]
-        #     else False
-        # )
-        # self._data = self._get_usdm()
-        # self._extra = self._get_extra()
 
     def excel(self, version: str = "4"):
         usdm_fullpath, _, _ = self._files.path("usdm")
@@ -32,18 +25,3 @@ class USDMDatabase:
         ue = USDM3Excel() if version == "3" else USDM4Excel()
         ue.to_excel(usdm_fullpath, excel_fullpath)
         return excel_fullpath, excel_filename, "application/vnd.ms-excel"
-
-    # def _get_usdm(self):
-    #     fullpath, filename, exists = self._files.path("usdm")
-    #     data = open(fullpath)
-    #     return json.load(data)
-
-    # def _get_raw(self):
-    #     fullpath, filename, exists = self._files.path("usdm")
-    #     f = open(fullpath)
-    #     return f.read()
-
-    # def _get_extra(self):
-    #     fullpath, filename, exists = self._files.path("extra")
-    #     data = open(fullpath)
-    #     return yaml.load(data, Loader=yaml.FullLoader)
